ce84583b925e38fd45bcc6dc7049e13c65b892e0
oguzhandilber <oguzhanndilber@gmail.com>
2021-07-04 14:05:29 +0300

    Updated config files

--- project-deployment.sh
+++ project-deployment.sh
@@ -16,6 +16,6 @@ echo "Creating the flask deployment and service..."
 kubectl apply -f ./KubernetesYamls/dev/flaskapp-deployment.yaml
 
 echo "Configuring MySQL database..."
-kubectl run -it --rm --image=mysql --restart=Never mysql-client -- mysql --host mysql --password=123456 -e "CREATE DATABASE flaskapi;USE flaskapi;INSERT INTO users(user_name, user_email, user_password ) VALUES ('oguzhan', 'k8s48@gmail.com', '123456');"
+kubectl run -it --rm --image=mysql --restart=Never mysql-client -- mysql --host mysql --password=123456 -e "CREATE DATABASE IF NOT EXISTS flaskapi;USE flaskapi;INSERT INTO users(user_name, user_email, user_password ) VALUES ('oguzhan', 'k8s48@gmail.com', '123456');"
 
 echo "Flask app created in the specified Namespace successfully!"
