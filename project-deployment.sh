56a0a9fab2737a92ad4179d6ec1273e39710be40
oguzhandilber <oguzhanndilber@gmail.com>
2021-07-04 14:00:27 +0300

    Changed Shell

---
+++ project-deployment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Creating the Namespace..."
+kubectl apply -f ./KubernetesYamls/dev/create-ns.yaml
+
+echo "Creating the volume..."
+kubectl apply -f ./KubernetesYamls/dev/mysql-pv.yaml
+
+echo "Creating the Database credentials..."
+kubectl apply -f ./KubernetesYamls/dev/flaskapi-secrets.yaml
+
+echo "Creating the mysql deployment and service..."
+kubectl apply -f ./KubernetesYamls/dev/mysql-deployment.yaml
+
+echo "Creating the flask deployment and service..."
+kubectl apply -f ./KubernetesYamls/dev/flaskapp-deployment.yaml
+
+echo "Configuring MySQL database..."
+kubectl run -it --rm --image=mysql --restart=Never mysql-client -- mysql --host mysql --password=123456 -e "CREATE DATABASE flaskapi;USE flaskapi;INSERT INTO users(user_name, user_email, user_password ) VALUES ('oguzhan', 'k8s48@gmail.com', '123456');"
+
+echo "Flask app created in the specified Namespace successfully!"
---
+++ sql-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'Execute SQL Script ...'
+
+`mysql -h "CREATE DATABASE flaskapi;
+USE flaskapi;
+CREATE TABLE users(user_id INT PRIMARY KEY AUTO_INCREMENT, user_name VARCHAR(255), user_email VARCHAR(255), user_password VARCHAR(255));"`
+
+echo 'Done Successfully.'
