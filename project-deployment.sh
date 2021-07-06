07995d0b6345217aa18a28777ea34c1a2c3a4313
Oğuzhan Dilber <61652081+oguzhandilber@users.noreply.github.com>
2021-07-06 19:27:45 +0300

    Update project-deployment.sh

--- project-deployment.sh
+++ project-deployment.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+$environment = "dev" # You can write test or prod for other namespaces. 
+
 echo "Creating the Namespace..."
-kubectl apply -f ./KubernetesYamls/dev/create-ns.yaml
+kubectl apply -f ./KubernetesYamls/$environment/create-ns.yaml
 
 echo "Creating the volume..."
-kubectl apply -f ./KubernetesYamls/dev/mysql-pv.yaml
+kubectl apply -f ./KubernetesYamls/$environment/mysql-pv.yaml
 
 echo "Creating the Database credentials..."
-kubectl apply -f ./KubernetesYamls/dev/flaskapi-secrets.yaml
+kubectl apply -f ./KubernetesYamls/$environment/flaskapi-secrets.yaml
 
 echo "Creating the mysql deployment and service..."
-kubectl apply -f ./KubernetesYamls/dev/mysql-deployment.yaml
+kubectl apply -f ./KubernetesYamls/$environment/mysql-deployment.yaml
 
 echo "Creating the flask deployment and service..."
-kubectl apply -f ./KubernetesYamls/dev/flaskapp-deployment.yaml
+kubectl apply -f ./KubernetesYamls/$environment/flaskapp-deployment.yaml
 
 echo "Configuring MySQL database..."
-kubectl run -it --rm --image=mysql --restart=Never mysql-client -- mysql --host mysql --password=123456 -e "CREATE DATABASE IF NOT EXISTS flaskapi;USE flaskapi;INSERT INTO users(user_name, user_email, user_password ) VALUES ('oguzhan', 'k8s48@gmail.com', '123456');"
+kubectl run -it -n $environment --rm --image=mysql --restart=Never mysql-client -- mysql --host mysql --password=123456 -e "CREATE DATABASE IF NOT EXISTS flaskapi;USE flaskapi;CREATE TABLE IF NOT EXISTS users(user_id INT PRIMARY KEY AUTO_INCREMENT, user_name VARCHAR(255), user_email VARCHAR(255), user_password VARCHAR(255));INSERT INTO users(user_name, user_email, user_password ) VALUES ('oguzhan', 'k8s48@gmail.com', '123456');"
 
 echo "Flask app created in the specified Namespace successfully!"
