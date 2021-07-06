40c35355c71f6c02ed05559ad7a7a95151a1d4df
Oğuzhan Dilber <61652081+oguzhandilber@users.noreply.github.com>
2021-07-06 15:10:47 +0300

    Update flaskapi.py

--- flaskapi.py
+++ flaskapi.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 mysql = MySQL()
 
 # MySQL configurations
-app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_USER"] = os.getenv("MYSQL_USERNAME")
 app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
 app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
 app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
